9c43e88422bc2801593781293acdbc26e89f69e9
p1v0zavr1k228 <60043091+AlexKeeper01@users.noreply.github.com>
2024-12-10 08:58:45 +0300

    Add files via upload

---
+++ pract16/main/java/com/example/demo/model/Feedback.java
@@ -0,0 +1,13 @@
+package com.example.demo.model;
+
+public class Feedback {
+    private String message;
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
